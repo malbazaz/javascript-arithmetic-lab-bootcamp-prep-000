642649d50327fb4ddd0e2c89eade0bd3010c6960
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-02 22:46:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function multiply(a,b){
 function divide (a,b){
   return a/b
 }
+function inc(n){
+  return n++
+}
+function dec(n){
+  return n--
+}
+function makeInt(n){
+  return parseInt(n)
+}
+function makeInt(n)
